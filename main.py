77214e9105c6b7963dba07b342c09d4d9a7fb610
amnadilshad3084 <amnadilshad905@gmail.com>
2026-02-09 22:30:48 +0500

    main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ matplotlib.use('TkAgg')
 
 from dashboard import show_dashboard
 
-if __name__ == "__main__":
+if __name__ == "__main__": #run dashboard only when this file is started, not when its imported
     show_dashboard()
